d5d84cb76cae625e37bffba2a9be467db93352a0
unyubgtina <innadosinaen@gmail.com>
2025-01-05 14:26:55 +0700

    baru

--- database/migrations/2025_01_05_065016_add_role_to_users_table.php
+++ database/migrations/2025_01_05_065016_add_role_to_users_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('login', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            //
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('login');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
--- database/migrations/2025_01_05_065036_create_dokter_table.php
+++ database/migrations/2025_01_05_065036_create_dokter_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('_data_user', function (Blueprint $table) {
+        Schema::create('dokter', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('_data_user');
+        Schema::dropIfExists('dokter');
     }
 };
