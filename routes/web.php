dea351cf9a921e58c4bcd1b6d0b14c44f39908ca
unyubgtina <innadosinaen@gmail.com>
2025-01-05 14:34:23 +0700

    baru lagi

--- routes/web.php
+++ routes/web.php
@@ -18,46 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+// Halaman Dashboard Admin
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
 
-Route::post('/login', function (){
-})->name('login');
+// CRUD Dokter
+Route::resource('/admin/dokter', DokterController::class);
 
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-});
+// Halaman Dashboard Pasien
+Route::get('/pasien/dashboard', [PasienController::class, 'dashboard']);
 
-Route::get('/login/dashboard', function () {
-    return view('dashboard');
-});
-
-Route::get('/login/dashboard/dataUser', function () {
-    return view('dataUser');
-});
-
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/dataUser', [DataUserController::class, 'index'])->name('admin.dataUser');
-});
-
-// routes/web.php
-
-use App\Http\Controllers\UserController;
-
-Route::resource('users', UserController::class);
-
-// Rute untuk menampilkan form data user
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-
-// Rute untuk menyimpan data user baru
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-
-// Rute untuk menampilkan form edit user
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-
-// Rute untuk mengupdate user
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-
-// Rute untuk menghapus user
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+// Booking Dokter
+Route::get('/pasien/booking', [PasienController::class, 'booking']);
